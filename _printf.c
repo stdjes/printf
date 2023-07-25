9857eee780e6cdc996a74aa51591743389177bd7
Michael <addictedchrysler@gmail.com>
2023-07-25 11:17:18 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -3,47 +3,53 @@
 #include <stdio.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - a custom function similar to the standard library function printf
+ * It generates output based on a given format string.
+ * @format: The format string containing characters and conversion specifiers.
+ *
+ * Description:
+ * This function utilizes the set_print() function to determine
+ * the appropriate printing function based on the conversion specifiers
+ * added to the format string.
+ *
+ * Return: total length of string of the formatted output
  */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *ind; /* index pointer/counter */
-	va_list arguments;
+	int (*pr_funcs)(va_list, flags_t *);
+	const char *chr;
+	va_list arg_list;
 	flags_t flags = {0, 0, 0};
 
-	register int count = 0;
+	double count = 0;
 
-	va_start(arguments, format);
+	va_start(arg_list, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (ind = format; *ind; ind++)
+	for (chr = format; *chr; chr++)
 	{
-		if (*ind == '%')
+		if (*chr == '%')
 		{
-			ind++;
-			if (*ind == '%')
+			chr++;
+			if (*chr == '%')
 			{
 				count += _putchar('%');
 				continue;
 			}
-			while (get_flag(*ind, &flags))
-				ind++;
-			pfunc = get_print(*ind);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *ind);
+			while (call_flag(*chr, &flags))
+				chr++;
+			pr_funcs = set_print(*chr);
+			count += (pr_funcs)
+				? pr_funcs(arg_list, &flags)
+				: _printf("%%%c", *chr);
 		} else
-			count += _putchar(*ind);
+			count += _putchar(*chr);
 	}
 	_putchar(-1);
-	va_end(arguments);
+	va_end(arg_list);
 	return (count);
+
 }
