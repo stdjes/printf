5b8643303213e72ebf97a443853b33ac6ef76949
Michael <addictedchrysler@gmail.com>
2023-07-25 11:20:58 -0700

    puts file

---
+++ _puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: Pointer to the string to print.
+ *
+ * Return: Number of characters written.
+ */
+int _puts(char *str)
+{
+	char *ptr = str;
+	int count = 0;
+
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+		count++;
+	}
+
+	return (count);
+}
+
