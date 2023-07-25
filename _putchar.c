a3a65502a3dd22d997dec5bea7eb9f710fb29096
Michael <addictedchrysler@gmail.com>
2023-07-25 11:19:49 -0700

    putchar file

--- _putchar.c
+++ _putchar.c
@@ -2,28 +2,31 @@
 #include <unistd.h>
 
 /**
- * _putchar - a function that writes the character c
- * to the standard output
+ * _putchar - Writes the character c to standard out.
+ * @c: The character to be written out
  *
- * @c: The character to be printed
- * Return: On success 1, returns -1 on error.
- * Description: _putchar is a customr function that
- * uses a local buffer of 1024 to call write function.
+ * Return: On success 1. On error, -1 is returned,
+ * Description: this _putchar  function uses a local buffer
+ * to call write as little s
+ * The buffer is flushed when it reaches its capacity
+ * or when a special flag value (-1) is passed.
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
+	static char buffer[1024];
+	static int buffer_index;
 
-	if (c == -1 || i >= 1024)
+	if (c == -1 || buffer_index >= 1023)
 	{
-		write(1, &buf, i);
-		i = 0;
+		write(1, buffer, buffer_index);
+		buffer_index = 0;
 	}
+
 	if (c != -1)
 	{
-		buf[i] = c;
-		i++;
+		buffer[buffer_index] = c;
+		buffer_index++;
 	}
+
 	return (1);
 }
